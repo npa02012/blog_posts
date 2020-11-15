51fd069cd3ed74c569b7ea41c5b0aa650a26fb74
npa02012 <npa02012@gmail.com>
2020-11-15 17:59:10 -0500

    ubuntu_setup fix

--- k8s_aws_setup/resources/ubuntu_setup.sh
+++ k8s_aws_setup/resources/ubuntu_setup.sh
@@ -20,14 +20,12 @@ sudo add-apt-repository \
 sudo apt-get install docker-ce docker-ce-cli containerd.io
    
 # Install kOps
-curl -Lo kops https://github.com/kubernetes/kops/releases/download/\
-		$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
+curl -Lo kops https://github.com/kubernetes/kops/releases/download/\$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
 chmod +x ./kops
 sudo mv ./kops /usr/local/bin/
 
 # Install kubectl
-curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/\
-		$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/\$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 
